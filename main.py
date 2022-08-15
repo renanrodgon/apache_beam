b227c5817cd1bde9a3e9e90380e971f292b0df32
renanrodgon <renanrodgon@yahoo.com.br>
2022-08-14 22:24:10 -0300

    leitura dos arquivos e transaformação em lista

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-import apache_beam
+from unittest import skip
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.options.pipeline_options import PipelineOptions
 
-print('Apache beam importado!')
-print(dir(apache_beam))
+pipeline_options =PipelineOptions(argv=None)
+pipeline = beam.Pipeline(options=pipeline_options)
+
+def texto_para_lista(elemento, delimitador='|'):
+    """
+    Recebe um texto e um delimitador
+    Retorna uma lista de elementos pelo delimitador
+    """
+
+    return elemento.split(delimitador)
+
+dengue = (
+    pipeline
+    | "Leitura do dataset de dengue" >> 
+        ReadFromText('casos_dengue.txt', skip_header_lines=1)
+    | "De texto para lista" >> beam.Map(texto_para_lista)
+    | "Mostrar resultados" >> beam.Map(print)
+
+)
+
+pipeline.run()
